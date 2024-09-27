05b42caffac349a8aecb0e987fa0bdaf4e34058d
Sudhakar Anemu <sudhakar.anemu@gmail.com>
2024-09-27 04:46:40 -0400

    446

--- .github/workflows/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = "ami-05938fdb1ee29ea96" # Replace with the AMI ID of your template
-  instance_type = "t2.micro"
-  key_name     = "jssepaws2024sudhaeai"
-
-  tags = {
-    Name = "mqtest1"
-  }
-}
